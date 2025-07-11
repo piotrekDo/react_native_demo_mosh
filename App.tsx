104fba63bf59c45e5f7d020b1e74976dd95c150d
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2025-07-11 19:04:48 +0200

    fundamentials section

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, SafeAreaView, Image, TouchableOpacity, TouchableHighlight, Button } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+      colors={['rgba(30,30,60,0.95)', 'rgba(10,10,20,1)']}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <StatusBar style='light' />
+      <SafeAreaView style={styles.container}>
+        <TouchableHighlight>
+          <Text numberOfLines={1} style={{ color: '#fff', fontSize: 24 }}>
+            Hello, World! a very very long text to see what happens when its too long!
+          </Text>
+        </TouchableHighlight>
+        <Image source={require('./assets/icon.png')} style={{ width: 200, height: 200 }} />
+        <TouchableOpacity>
+          <Image source={{ uri: 'https://picsum.photos/id/237/200/300' }} style={{ width: 200, height: 300 }} />
+        </TouchableOpacity>
+
+        <Button title="Press Me" onPress={() => alert('Button Pressed!')} />
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
-});
+  button: {
+    backgroundColor: '#6200ee',
+    padding: 10,
+}});
--- notki.md
+++ notki.md
@@ -3,3 +3,19 @@
 poprzednie CLI jest przestarzałe, dopisek --template pozwala wybrac opcje w postaci TS. Domyslnie tworzy sie blank bez TS
 
 npx create-expo-app DoneWithItMosh --template
+
+
+## komponenty, brak HTML
+
+native nie korzysta z html, zamiast tego wykorzystywane sa komponenty o podobnym zastosowaniu co elementy HTML
+
+Image – do wyświetlania obrazów.
+ScrollView – do przewijania zawartości.
+FlatList – do wydajnego wyświetlania list.
+TouchableOpacity, TouchableHighlight, Pressable – do obsługi dotyku (przyciski, reakcje na kliknięcia).
+TextInput – pole do wprowadzania tekstu (formularze).
+Button – prosty przycisk.
+SafeAreaView – zapewnia, że zawartość nie nachodzi na elementy systemowe (np. notcha).
+
+Dlaczego nie ma standardowych elementów HTML?
+React Native nie używa HTML ani DOM. Zamiast tego korzysta z własnych komponentów, które są tłumaczone na natywne widoki Androida/iOS. Dzięki temu aplikacje wyglądają i działają jak natywne, a nie jak strony internetowe.
