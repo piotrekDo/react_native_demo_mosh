f787f4d560ee4f9920ab4cf97030cf21bcb491c5
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2025-07-20 11:14:39 +0200

    reuasble app form

---
+++ app/components/AppForm.tsx
@@ -0,0 +1,40 @@
+import { Formik, FormikHelpers, FormikValues } from 'formik';
+import React, { ReactNode } from 'react';
+import { Keyboard, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import * as Yup from 'yup';
+import colors from '../config/colors';
+
+interface Props<T extends FormikValues> {
+  initialValues: T;
+  onSubmit: (values: T, formikHelpers: FormikHelpers<T>) => void;
+  validationSchema?: Yup.ObjectSchema<any>;
+  children: ReactNode;
+}
+
+export function AppForm<T extends FormikValues>({
+  initialValues,
+  onSubmit,
+  validationSchema,
+  children,
+}: Props<T>) {
+  return (
+    <Formik<T>
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {() => (
+          <View style={styles.container}>
+            {children}
+          </View>
+      )}
+    </Formik>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: colors.white,
+  },
+});
